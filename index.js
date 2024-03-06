b9e6ba0978be509be44359cf46daef7d1565fe13
Luanr Nazairua <jlsivan2008@gmail.com>
2024-03-06 11:01:40 -0500

    Lots more progress

---
+++ index.js
@@ -0,0 +1,15 @@
+const catImages = [
+    "Images/Luna.jpg",
+    "Images/Mocha.jpg",
+    "Images/Shrimp.jpg",
+    "Images/_.jpg",
+    //"Images/_.jpg",
+]
+
+function selected(option) {
+    document.getElementById('testing').src = catImages[option]
+}
+
+function yuriMode() {
+    //TIME
+}
