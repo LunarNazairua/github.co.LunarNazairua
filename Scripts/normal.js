5a6a4884ef3d3be5e00485413136d8d28ce86fd7
Luanr Nazairua <jlsivan2008@gmail.com>
2024-03-10 17:07:01 -0400

    being sill

---
+++ Scripts/normal.js
@@ -0,0 +1,11 @@
+const classified = [
+    "THERE IS SOMETING IN THE WALLS",
+    "Thats me lol",
+    "oh... Whu are you in my walls?",
+    "Cuz I pay rent, why do you think you keep finding money near the vent every bill day",
+    "OHHHH so that was you!!!!! Alright I'll let you stay there buddy",
+    "Thanks bro"
+]
+
+
+function temp(index) {}
